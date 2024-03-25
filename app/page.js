4c3d0f65ed59e835bff2263c8d603a41a5b89ba7
franclobo <fjbl2788@gmail.com>
2024-03-25 13:14:18 -0500

    Solve FAQs challenge

--- app/page.js
+++ app/page.js
@@ -1,113 +1,81 @@
 import Image from "next/image";
+import Start from "../public/assets/images/icon-star.svg";
+import Minus from "../public/assets/images/icon-minus.svg";
+import Plus from "../public/assets/images/icon-plus.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main className="content-container flex flex-col items-left justify-between">
+      <h1 className="flex text-4xl font-bold mb-10">
+        <Image src={Start} alt="Start" className="mr-10"/>
+        FAQs
+      </h1>
+      <div className="flow-root">
+  <div className="-my-8 divide-y divide-gray-100">
+    <details className="group py-8 [&_summary::-webkit-details-marker]:hidden" open>
+      <summary className="grayish-purple flex cursor-pointer items-center justify-between text-gray-900">
+        <h2 className="text-lg font-medium">What is Frontend Mentor, and how will it help me?</h2>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <span className="relative size-5 shrink-0">
+          <Image src={Minus} alt="Minus" className="absolute inset-0 size-5 opacity-0 group-open:opacity-100" />
+          <Image src={Plus} alt="Plus" className="absolute inset-0 size-5 opacity-100 group-open:opacity-0" />
+        </span>
+      </summary>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <p className="mt-4 leading-relaxed text-gray-700">
+      Frontend Mentor offers realistic coding challenges to help developers improve their 
+  frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for 
+  all levels and ideal for portfolio building.
+      </p>
+    </details>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    <details className="group py-8 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="grayish-purple flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+        <h2 className="text-lg font-medium">Is Frontend Mentor free?</h2>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <span className="relative size-5 shrink-0">
+        <Image src={Minus} alt="Minus" className="absolute inset-0 size-5 opacity-0 group-open:opacity-100" />
+          <Image src={Plus} alt="Plus" className="absolute inset-0 size-5 opacity-100 group-open:opacity-0" />
+        </span>
+      </summary>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <p className="mt-4 leading-relaxed text-gray-700">
+      Yes, Frontend Mentor offers both free and premium coding challenges, with the free 
+  option providing access to a range of projects suitable for all skill levels.
+      </p>
+    </details>
+    <details className="group py-8 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="grayish-purple flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+        <h2 className="text-lg font-medium">Can I use Frontend Mentor projects in my portfolio?</h2>
+
+        <span className="relative size-5 shrink-0">
+        <Image src={Minus} alt="Minus" className="absolute inset-0 size-5 opacity-0 group-open:opacity-100" />
+          <Image src={Plus} alt="Plus" className="absolute inset-0 size-5 opacity-100 group-open:opacity-0" />
+        </span>
+      </summary>
+
+      <p className="mt-4 leading-relaxed text-gray-700">
+      Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+  way to showcase your skills to potential employers!
+      </p>
+    </details>
+    <details className="group py-8 [&_summary::-webkit-details-marker]:hidden">
+      <summary className="grayish-purple flex cursor-pointer items-center justify-between gap-1.5 text-gray-900">
+        <h2 className="text-lg font-medium">How can I get help if I'm stuck on a Frontend Mentor challenge?</h2>
+
+        <span className="relative size-5 shrink-0">
+        <Image src={Minus} alt="Minus" className="absolute inset-0 size-5 opacity-0 group-open:opacity-100" />
+          <Image src={Plus} alt="Plus" className="absolute inset-0 size-5 opacity-100 group-open:opacity-0" />
+        </span>
+      </summary>
+
+      <p className="mt-4 leading-relaxed text-gray-700">
+      The best place to get help is inside Frontend Mentor's Discord community. There's a help 
+  channel where you can ask questions and seek support from other community members.
+      </p>
+    </details>
+  </div>
+</div>
     </main>
   );
 }
